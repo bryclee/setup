e6030cf3579aa3631e93a49eb8fab0df9a9cedaf
Bryan Lee <bryalee@paypal.com>
2018-08-31 11:02:42 -0700

    Update link script, add tmux.conf to link

--- link.sh
+++ link.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+function safelyLink {
+  for file in "$@"
+  do 
+    target_file=~/$file
+
+    if [ -e "$target_file" ]; then
+      # If it has no size (invalid or empty)
+      if [ ! -s "$target_file" ]; then
+        rm $target_file
+        # If it's a symlink
+      elif [ -L "$target_file" ]; then
+        echo "$target_file already linked, skipping"
+        continue
+      else
+        echo "$target_file exists, creating backup $target_file.bak"
+        mv $target_file $target_file.bak
+      fi
+    fi
+
+    ln -sv $(pwd)/$file ~/
+  done
+}
+
 # Link files here
-ln -sv $(pwd)/.vim ~/
-ln -sv $(pwd)/.inputrc ~/
-ln -sv $(pwd)/.gitconfig ~/
-ln -sv $(pwd)/.bash_profile ~/
-ln -sv $(pwd)/.bash_aliases ~/
-ln -sv $(pwd)/.profile ~/
-ln -sv $(pwd)/.bashrc ~/
-ln -sv $(pwd)/.tmux.conf ~/
+safelyLink .vim .inputrc .gitconfig .bash_profile .bash_aliases .profile .bashrc .tmux.conf
