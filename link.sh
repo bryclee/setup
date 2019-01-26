01b0b01d0e567315930bc7f65793e9098c72ee41
Bryan Lee <pygmize.me@gmail.com>
2019-01-25 17:26:23 -0800

    [nvim] Commit nvim settings

--- link.sh
+++ link.sh
@@ -10,3 +10,7 @@ function safelyLink {
 # Link files here
 safelyLink .vim .inputrc .gitconfig .bash_profile .bash_aliases .profile .bashrc \
     .tmux.conf .tmux.macosx .tmux.linux
+
+mkdir -p ~/.config
+
+./safelyLink.sh nvim ~/.config
---
+++ nvim/init.vim
@@ -0,0 +1,6 @@
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath =&runtimepath
+source ~/.vim/vimrc
+
+" Live preview substitute command results
+set inccommand=split
