fb125c08f83914cd1a76bd97ea25086ba2aff092
Bryan Lee <bryalee@paypal.com>
2019-01-31 22:01:26 -0800

    [slate] Finish up basic key bindings

--- .slate.js
+++ .slate.js
@@ -1,34 +1,58 @@
-const HALF_WIDTH = 'screenSizeX/2'
-
-const dockLeft = slate.operation('move', {
+const pushLeft = slate.operation('move', {
     x: 0,
     y: 0,
     width: 'screenSizeX/2',
     height: 'screenSizeY'
 });
-const dockRight = slate.operation('move', {
+const pushRight = slate.operation('move', {
     x: 'screenSizeX/2',
     y: 0,
     width: 'screenSizeX/2',
     height: 'screenSizeY'
-})
-const dockTop = slate.operation('move', {
+});
+const halfTop = slate.operation('move', {
     x: 0,
     y: 0,
     width: 'screenSizeX',
     height: 'screenSizeY/2'
-})
-const dockBottom = slate.operation('move', {
+});
+const halfBottom = slate.operation('move', {
     x: 0,
     y: 'screenSizeY/2',
     width: 'screenSizeX',
     height: 'screenSizeY/2'
-})
+});
+const maximize = slate.operation('move', {
+    x: 0,
+    y: 0,
+    width: 'screenSizeX',
+    height: 'screenSizeY'
+});
+
+const downsize = slate.operation('move', {
+    x: 'windowTopLeftX+(windowSizeX*0.05)',
+    y: 'windowTopLeftY+(windowSizeY*0.05)',
+    width: 'windowSizeX*0.9',
+    height: 'windowSizeY*0.9'
+});
+
+const throwRight = slate.operation('throw', {
+    screen: 'right'
+});
+const throwLeft = slate.operation('throw', {
+    screen: 'left'
+});
 
 slate.bindAll({
-    'left:ctrl,cmd': dockLeft,
-    'right:ctrl,cmd': dockRight,
-    'up:ctrl,cmd': dockTop,
-    'down:ctrl,cmd': dockBottom,
-    'r:ctrl,cmd': slate.operation('relaunch')
+    '[:ctrl,cmd': pushLeft,
+    ']:ctrl,cmd': pushRight,
+    '=:ctrl,cmd': maximize,
+    '-:ctrl,cmd': downsize,
+
+    '[:ctrl,cmd,alt': throwLeft,
+    ']:ctrl,cmd,alt': throwRight,
+
+    'g:ctrl,cmd': slate.operation('grid'),
+
+    'r:ctrl,shift': slate.operation('relaunch')
 });
