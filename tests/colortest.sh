4743fb5a9ea0f20c40b1b1d397d348cd5496327e
Bryan Lee <bryalee@paypal.com>
2019-05-03 21:36:09 -0700

    [colortest] Print 16 color

--- tests/colortest.sh
+++ tests/colortest.sh
@@ -1,13 +1,14 @@
-printf "          "
-for b in 0 1 2 3 4 5 6 7; do printf "  4${b}m "; done
+printf "        "
+for b in 0 1 2 3 4 5 6 7; do printf "  4${b}m"; done
+for b in 0 1 2 3 4 5 6 7; do printf " 10${b}m"; done
 echo
-for f in "" 30 31 32 33 34 35 36 37; do
-  for s in "" "1;"; do
-    printf "%4sm" "${s}${f}"
-    printf " \033[%sm%s\033[0m" "$s$f" "gYw "
+for f in "" 30 31 32 33 34 35 36 37 90 91 92 93 94 95 96 97; do
+  printf "%3sm " "${f}"
+  printf "\033[%sm%s\033[0m" "$f" "gYw "
+  for bb in 4 10; do
     for b in 0 1 2 3 4 5 6 7; do
-      printf " \033[4%s;%sm%s\033[0m" "$b" "$s$f" " gYw "
+      printf "\033[%s;%sm%s\033[0m" "$bb$b" "$f" " gYw "
     done
-    echo
   done
+  echo
 done
