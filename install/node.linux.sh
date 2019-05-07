7b3f63fecf232ee0fdcddbe7084b2536e6fdc79d
Bryan Lee <pygmize.me@gmail.com>
2019-05-06 23:56:05 -0700

    [setup] Fix sourcing nvm during initial setup

--- install/node.linux.sh
+++ install/node.linux.sh
@@ -1,7 +1,8 @@
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
-# # for now, default version will be node 8
+# for now, default version will be node 8
+. ~/.nvm/nvm.sh
+. ~/.nvm/bash_completion
 nvm install 8
 nvm alias default 8
 ./safelyLink.sh .nvm/default-packages ~/.nvm
