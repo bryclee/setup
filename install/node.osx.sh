7d6835fa6792814e0e5cec85e066840df0aca94c
Bryan Lee <bryalee@paypal.com>
2021-10-07 23:38:21 -0700

    add sessionizer to vimrc, update nvm version

--- install/node.osx.sh
+++ install/node.osx.sh
@@ -1,9 +1,7 @@
 mkdir ~/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-# # for now, default version will be node 8
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash
 . ~/.nvm/nvm.sh
 . ~/.nvm/bash_completion
 nvm install 12
 nvm alias default 12
 ./safelyLink.sh .nvm/default-packages ~/.nvm
-
