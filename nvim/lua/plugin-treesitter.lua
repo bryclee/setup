7e67a06f4d56ff3f7fab14e5811d7ce6423f9d12
Bryan Lee <bryalee@paypal.com>
2021-10-19 07:52:32 -0700

    Remove treesitter indent

--- nvim/lua/plugin-treesitter.lua
+++ nvim/lua/plugin-treesitter.lua
@@ -1,14 +1,14 @@
 require'nvim-treesitter.configs'.setup {
-  indent = {
-    enable = true
-  },
+  -- indent = {
+  --   enable = true
+  -- },
   highlight = {
     enable = true
   },
-  incremental_selection = {
-    enable = true
-  },
-  textobjects = {
-    enable = true
-  },
+  -- incremental_selection = {
+  --   enable = true
+  -- },
+  -- textobjects = {
+  --   enable = true
+  -- },
 }
