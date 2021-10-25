b4362d2342f0b31d08eaeddcd7f3d3680bc7e79a
Bryan Lee <pygmize.me@gmail.com>
2021-10-25 10:39:10 -0700

    [tests] Fix colortest printing default color with background

--- tests/colortest.sh
+++ tests/colortest.sh
@@ -7,7 +7,7 @@ for f in "" 30 31 32 33 34 35 36 37 90 91 92 93 94 95 96 97; do
   printf "\033[%sm%s\033[0m" "$f" "gYw "
   for bb in 4 10; do
     for b in 0 1 2 3 4 5 6 7; do
-      printf "\033[%s;%sm%s\033[0m" "$bb$b" "$f" " gYw "
+      printf "\033[%s;%sm%s\033[0m" "$f" "$bb$b" " gYw "
     done
   done
   echo
