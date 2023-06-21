b5a71242040bbd5db4de90d9b8d728abd29b059f
Bryan Lee <pygmize.me@gmail.com>
2023-06-21 11:36:35 -0700

    update kitty settings and ubuntu key repeat settings

--- install/misc.linux.sh
+++ install/misc.linux.sh
@@ -3,6 +3,6 @@ sudo apt-get install -y silversearcher-ag bash-completion ripgrep fzf
 # For gnome desktop, set key repeat rate
 if [ -n "$(which gsettings)" ]
 then
-  gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
-  gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+  gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15
+  gsettings set org.gnome.desktop.peripherals.keyboard delay 150
 fi
