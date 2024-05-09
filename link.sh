fc00d6c8c4cea4cd0c132976c8282be44dc02506
Bryan Lee <pygmize.me@gmail.com>
2024-05-09 08:39:14 -0700

    Rename .vim to vim

--- link.sh
+++ link.sh
@@ -8,13 +8,14 @@ function safelyLink {
 }
 
 # Link files from here to home dir
-safelyLink .vim .inputrc .gitconfig .bash_profile .bash_aliases .profile .bashrc .zshrc \
+safelyLink .inputrc .gitconfig .bash_profile .bash_aliases .profile .bashrc .zshrc \
     .tmux.conf .agignore .gitignore_global .slate.js .hyper.js .prettierrc.js .eslintrc.js \
     .hammerspoon
 
 mkdir -p ~/.config
 mkdir -p ~/.nvm
 
+./safelyLink.sh vim ~/.vim
 ./safelyLink.sh nvim ~/.config
 ./safelyLink.sh kitty ~/.config
 ./safelyLink.sh .nvm/default-packages ~/.nvm
