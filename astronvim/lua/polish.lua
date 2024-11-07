6d148ed40f0efe8c7dddd7668f906f9d0ee188db
Bryan Lee <bryalee@paypal.com>
2024-11-06 23:02:32 -0800

    Add quick close for fugitive buffers

--- astronvim/lua/polish.lua
+++ astronvim/lua/polish.lua
@@ -23,4 +23,5 @@ if vim.fn.executable "rg" == 1 then vim.o.grepprg = "rg --vimgrep --smart-case" 
 -- Tmux-s to launch new session
 vim.keymap.set("n", "<C-f>", function() vim.fn.system { "tmux", "neww", "tmux-s" } end)
 
+-- Load local vimrc if present
 if vim.fn.filereadable "~/.vimrc_local" then vim.cmd.source "~/.vimrc_local" end
