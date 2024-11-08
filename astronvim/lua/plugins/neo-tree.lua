d1d20508c606befeecca2287ed7577059a7f1b7e
Bryan Lee <bryalee@paypal.com>
2024-11-08 15:27:54 -0800

    Allow changing dir from sidebar

--- astronvim/lua/plugins/neo-tree.lua
+++ astronvim/lua/plugins/neo-tree.lua
@@ -3,6 +3,8 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
     opts.window.mappings["/"] = "noop"
-    opts.filesystem.bind_to_cwd = false
+
+    opts.filesystem.cwd_target = opts.filesystem.cwd_target or {}
+    opts.filesystem.cwd_target.current = "none"
   end,
 }
