0504137073bb08a787204fcebb438c019c878d80
Bryan Lee <pygmize.me@gmail.com>
2025-03-30 08:01:15 -0700

    Update dashboard

--- astronvim5/lua/plugins/dashboard.lua
+++ astronvim5/lua/plugins/dashboard.lua
@@ -4,11 +4,26 @@ return {
   opts = function(_, opts)
     if not opts.dashboard then return {} end
 
-    table.insert(opts.dashboard.preset.keys, {
-      key = "-",
-      action = "<leader>-",
-      desc = "Explorer",
-      icon = " "
+    vim.list_extend(opts.dashboard.preset.keys, {
+      {
+        key = "-",
+        action = "<leader>-",
+        desc = "Explorer",
+        icon = " ",
+      },
+      {
+        key = "g",
+        action = "<leader>gg",
+        desc = "Lazy git",
+        icon = " ",
+      },
+      {
+        key = "a",
+        action = "<leader>oaa",
+        desc = "Org Agenda",
+        icon = " ",
+      },
+      { title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
     })
   end,
 }
