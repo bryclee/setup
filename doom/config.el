eb7fbe35ae423e36e493712f3527ff78e1681ef0
Bryan Lee <bryalee@paypal.com>
2025-09-12 23:16:49 -0700

    Update doom config to set default themes

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,24 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tomorrow-night)
+
+;; Set function to create new keybinding
+(defun my/toggle-theme ()
+  (interactive)
+  (if (eq doom-theme 'doom-tomorrow-night)
+     (progn
+       (setq doom-theme 'doom-tomorrow-day)
+       (disable-theme 'doom-tomorrow-night)
+       (load-theme 'doom-tomorrow-day))
+     (progn
+       (setq doom-theme 'doom-tomorrow-night)
+       (disable-theme 'doom-tomorrow-day)
+       (load-theme 'doom-tomorrow-night))))
+
+(map! :leader
+      (:prefix ("t" . "toggle")
+               :desc "Toggle theme" "T" #'my/toggle-theme))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
